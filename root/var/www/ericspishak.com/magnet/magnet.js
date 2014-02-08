cd44d51e59cc2a5d592a7bb5f49c2ce814e414b4
Eric Spishak <emspishak@gmail.com>
2014-02-08 19:49:16 +0000

    Clean up JS.

--- root/var/www/ericspishak.com/magnet/magnet.js
+++ root/var/www/ericspishak.com/magnet/magnet.js
@@ -1,9 +1,9 @@
-window.onload = function() {
+window.addEventListener('load', function() {
   var button = document.querySelector('#parse');
   button.addEventListener('click', handleParseClick);
-};
+});
 
 function handleParseClick() {
   var results = document.querySelector('#results');
   results.textContent = document.querySelector('#magnet-uri').value;
-};
+}
