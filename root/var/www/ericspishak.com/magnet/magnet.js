c20dd4e375d716cd0d3287afe7fc825bc33d1b68
Eric Spishak <emspishak@gmail.com>
2014-02-09 00:24:40 +0000

    Remove button, update when text is entered.

--- root/var/www/ericspishak.com/magnet/magnet.js
+++ root/var/www/ericspishak.com/magnet/magnet.js
@@ -1,11 +1,11 @@
 window.addEventListener('load', main);
 
 function main() {
-  var button = document.querySelector('#parse');
-  button.addEventListener('click', handleParseClick);
+  var textarea = document.querySelector('#magnet-uri');
+  textarea.addEventListener('input', handleMagnetUriInput);
 }
 
-function handleParseClick() {
+function handleMagnetUriInput() {
   var results = document.querySelector('#results');
   results.textContent = document.querySelector('#magnet-uri').value;
 }
