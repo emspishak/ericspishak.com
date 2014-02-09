7aa48d4a656fa4190c98734bff1c4227a7612013
Eric Spishak <emspishak@gmail.com>
2014-02-09 02:03:09 +0000

    Parse and output protocol.

--- root/var/www/ericspishak.com/magnet/magnet.js
+++ root/var/www/ericspishak.com/magnet/magnet.js
@@ -6,6 +6,45 @@ function main() {
 }
 
 function handleMagnetUriInput() {
+  var uri = document.querySelector('#magnet-uri').value;
+  var properties = parseMagnetUri(uri);
+  writeResults(properties);
+}
+
+function parseMagnetUri(uri) {
+  var properties = {};
+  var pos = 0;
+
+  var protocol = '';
+  while (pos < uri.length && uri.charAt(pos) != ':') {
+    protocol += uri.charAt(pos);
+    pos++;
+  }
+  properties['protocol'] = protocol;
+
+  return properties;
+}
+
+function writeResults(properties) {
   var results = document.querySelector('#results');
-  results.textContent = document.querySelector('#magnet-uri').value;
+  results.innerHTML = '';
+
+  var dl = document.createElement('dl');
+  var keys = Object.keys(properties);
+  for (var i = 0; i < keys.length; i++) {
+    var key = keys[i];
+    var value = properties[key];
+    if (value) {
+      var dt = document.createElement('dt');
+      dt.textContent = key;
+      var dd = document.createElement('dd');
+      dd.textContent = value;
+      dl.appendChild(dt);
+      dl.appendChild(dd);
+    }
+  }
+
+  if (dl.children.length > 0) {
+    results.appendChild(dl);
+  }
 }
