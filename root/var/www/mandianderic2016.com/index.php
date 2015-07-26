30f52f6178a0dfe7d9842534edbb84bc060f7dd4
Eric Spishak <emspishak@gmail.com>
2015-07-26 00:22:54 +0000

    Move common code to a template file.

---
+++ root/var/www/mandianderic2016.com/index.php
@@ -0,0 +1,21 @@
+<?php
+   include_once("template.php");
+   top();
+?>
+
+<img src="images/fall_ball.jpg" alt="Mandi and Eric" class="main-image">
+<p>
+  Come celebrate our wedding
+</p>
+<p>
+  Mandi Thomas and Eric Spishak
+</p>
+<p>
+  Saturday, April 30, 2016
+</p>
+<p>
+  Some short text about how great Mandi and Eric are, all the fun things they've done and how excited they are to get married.
+</p>
+<?php
+   footer();
+?>
--- root/var/www/mandianderic2016.com/template.php
+++ root/var/www/mandianderic2016.com/template.php
@@ -1,3 +1,6 @@
+<?php
+function top() {
+  ?>
 <!DOCTYPE html>
 <html>
   <head>
@@ -31,20 +34,15 @@
         </nav>
       </header>
       <main>
-        <img src="images/fall_ball.jpg" alt="Mandi and Eric" class="main-image">
-        <p>
-          Come celebrate our wedding
-        </p>
-        <p>
-          Mandi Thomas and Eric Spishak
-        </p>
-        <p>
-          Saturday, April 30, 2016
-        </p>
-        <p>
-          Some short text about how great Mandi and Eric are, all the fun things they've done and how excited they are to get married.
-        </p>
+  <?php
+}
+
+function footer() {
+  ?>
       </main>
     </div>
   </body>
 </html>
+  <?php
+}
+?>
