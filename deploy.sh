1a96fcab8786dd4a09305dafabc43e7b0b373294
Eric Spishak <emspishak@gmail.com>
2015-08-19 03:15:55 +0000

    Run Wedding website through php before copying it to serve only static content.

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,20 @@
-# Copies files to their correct locations
-
 #!/bin/bash
 
-rsync -r --exclude=*~ --inplace root/* /
+# Copies files to their correct locations
+
+WEDDING="var/www/mandianderic2016.com"
+WEDDING_WORKING="root/$WEDDING"
+
+rsync -r --exclude=*~ --exclude=$WEDDING --inplace root/* /
+rsync -r --exclude=*~ --exclude=*.php --inplace $WEDDING_WORKING/* /$WEDDING
+
+pushd root
+PHPS=`find $WEDDING -name "*.php"`
+for PHP in $PHPS; do
+    if [ $(basename $PHP) = "template.php" ]; then
+        continue
+    fi
+    HTML=`echo $PHP | sed 's/\.php$/.html/'`
+    php $PHP > /$HTML
+done
+popd
--- root/var/www/mandianderic2016.com/wedding/index.php
+++ root/var/www/mandianderic2016.com/wedding/index.php
@@ -1,5 +1,5 @@
 <?php
-include_once("../template.php");
+include_once(__DIR__ . "/../template.php");
 top(Page::WEDDING);
 ?>
 Wedding!
