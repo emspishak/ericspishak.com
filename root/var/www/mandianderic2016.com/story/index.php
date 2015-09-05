162d2e67493dd482f4e545ac9ef8a6ecf05aada6
Eric Spishak <emspishak@gmail.com>
2015-09-05 19:13:03 +0000

    Wrap text.

--- root/var/www/mandianderic2016.com/story/index.php
+++ root/var/www/mandianderic2016.com/story/index.php
@@ -4,16 +4,54 @@ top(Page::STORY);
 ?>
 <div class="story-image"></div>
 <p>
-  Eric and Mandi met the summer of 2009 through their mutual friend, John. Both invited to John's house one night, Mandi liked Eric's jeans and Eric liked everything about Mandi. The following weekend Mandi and Eric were scheduled to attend Seattle's 3-day music festival, Bumbershoot. As fate would have it, Eric would befriend Mandi on Facebook, Mandi would explain how much Eric reminds her of Michael Cera, and Eric would provide his digits to meet up at Bumbershoot. After spending an entire day listening to music together, making fun of and consequently making friends with Mandi's sister by using the infamous "excuse me, princess?" line, the two went home to text for several more hours ending the night with a couple hour phone call regarding Eric's love of Switzerland. For the record, I don't think either of us got any sleep that night.
+  Eric and Mandi met the summer of 2009 through their mutual friend,
+  John. Both invited to John's house one night, Mandi liked Eric's
+  jeans and Eric liked everything about Mandi. The following weekend
+  Mandi and Eric were scheduled to attend Seattle's 3-day music
+  festival, Bumbershoot. As fate would have it, Eric would befriend
+  Mandi on Facebook, Mandi would explain how much Eric reminds her of
+  Michael Cera, and Eric would provide his digits to meet up at
+  Bumbershoot. After spending an entire day listening to music
+  together, making fun of and consequently making friends with Mandi's
+  sister by using the infamous "excuse me, princess?" line, the two
+  went home to text for several more hours ending the night with a
+  couple hour phone call regarding Eric's love of Switzerland. For the
+  record, I don't think either of us got any sleep that night.
 </p>
 <p>
-  Their first date was spent at a restaurant Eric didn't realize was fancy (Il Fornaio) and going to the wrong movie theater for a really boring movie (<em>The Informant</em>). After a couple months of dating and becoming fast friends, Eric tricked Mandi into being his girlfriend (just kidding) by stating "I think we should be boyfriend/girlfriend".
+  Their first date was spent at a restaurant Eric didn't realize was
+  fancy (Il Fornaio) and going to the wrong movie theater for a really
+  boring movie (<em>The Informant</em>). After a couple months of
+  dating and becoming fast friends, Eric tricked Mandi into being his
+  girlfriend (just kidding) by stating "I think we should be
+  boyfriend/girlfriend".
 </p>
 <p>
-  Over 5½ years later, Eric decided enough was enough and bought Mandi something sparkly. Luckily for Eric, a trip to Kauai had been planned for Mandi's grandma's 80th birthday surprise and little did Mandi know, there was more than one surprise planned for the week. On a rainy, muddy, and absolutely perfect Na Pali Coast hike, just the two of them, Eric got down on one knee and asked Mandi to marry him. The two then celebrated by taking a necessary engagement selfie (we don't all need to have creepy photographers hiding in the bushes), calling all of their friends, eating the best fish tacos either of them had ever had at a shack on the drive home, and buying three bottles of champagne to share with Mandi's unsuspecting family.
+  Over 5½ years later, Eric decided enough was enough and bought Mandi
+  something sparkly. Luckily for Eric, a trip to Kauai had been
+  planned for Mandi's grandma's 80th birthday surprise and little did
+  Mandi know, there was more than one surprise planned for the
+  week. On a rainy, muddy, and absolutely perfect Na Pali Coast hike,
+  just the two of them, Eric got down on one knee and asked Mandi to
+  marry him. The two then celebrated by taking a necessary engagement
+  selfie (we don't all need to have creepy photographers hiding in the
+  bushes), calling all of their friends, eating the best fish tacos
+  either of them had ever had at a shack on the drive home, and buying
+  three bottles of champagne to share with Mandi's unsuspecting
+  family.
 </p>
 <p>
-  Since the beginning of their relationship, Mandi and Eric have sought out adventures together. From the simple adventure of commuting back and forth between Seattle University and the University of Washington on the exciting 48 and 49 bus routes to the grand adventure of backpacking through six South American countries. From a year of long distance Skype calls and six hour flights to summiting the tallest mountain in the continental U.S. and crying the last 2 miles (it's obvious who was crying right? Eric not Mandi...). They have had the most unforgettable six years and are ecstatic, anxious, and overjoyed to spend the rest of their lives together.
+  Since the beginning of their relationship, Mandi and Eric have
+  sought out adventures together. From the simple adventure of
+  commuting back and forth between Seattle University and the
+  University of Washington on the exciting 48 and 49 bus routes to the
+  grand adventure of backpacking through six South American
+  countries. From a year of long distance Skype calls and six hour
+  flights to summiting the tallest mountain in the continental
+  U.S. and crying the last 2 miles (it's obvious who was crying right?
+  Eric not Mandi...). They have had the most unforgettable six years
+  and are ecstatic, anxious, and overjoyed to spend the rest of their
+  lives together.
 </p>
 <?php
 footer();
