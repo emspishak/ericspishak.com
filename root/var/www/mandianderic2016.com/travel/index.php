48a91c4d1a470f15b2e8f77f03a0c9dc396cde8b
Eric Spishak <emspishak@gmail.com>
2015-11-08 02:51:40 +0000

    Text for the Travel page.

--- root/var/www/mandianderic2016.com/travel/index.php
+++ root/var/www/mandianderic2016.com/travel/index.php
@@ -2,7 +2,87 @@
 include_once(__DIR__ . "/../template.php");
 top(Page::TRAVEL);
 ?>
-Travel
+<p>
+  Yosemite National Park is an approximately 3-4 hour drive from multiple
+  airports in the area. Here's how you get to us:
+</p>
+<div>
+  <div>
+    <h2>
+      Step 1: Fly
+    </h2>
+    <p>
+      Your airport options are:
+    </p>
+    <ul>
+      <li>
+        Oakland International Airport
+      </li>
+      <li>
+        San Francisco International Airport
+      </li>
+      <li>
+        Mineta San Jose International Airport
+      </li>
+      <li>
+        Sacramento International Airport
+      </li>
+      <li>
+        Fresno Yosemite International Airport
+      </li>
+    </ul>
+    <p>
+      We recommend <strong>Oakland International Airport</strong> because it's a
+      large aiport and it avoids the unpredictability and nightmare that is the
+      Bay Bridge traffic if you fly into San Francisco International Airport.
+    </p>
+    <p>
+      Each airport has its pros and cons so we encourage you to do your research
+      and find the most convenient, easiest, and cheapest flights possible. For
+      example, Fresno or Sacramento may have less traffic than leaving from one
+      of the central Bay Area airports. Don’t hesitate to reach out to us when
+      booking your travel so we can help make it easier for you.
+    </p>
+  </div>
+  <div>
+    <h2>
+      Step 2: Rent a car
+    </h2>
+    <p>
+      All airports have car rental facilities.
+    </p>
+    <p>
+      Pairing up with or organizing a group to carpool to Yosemite is a great
+      way to save money on renting a car AND have double or triple the fun on
+      the mini roadtrip!
+    </p>
+    <p>
+      If you would like to do this, but are unsure of who else is going or need
+      to be paired by a professional (i.e. Eric or Mandi) we are happy to
+      assist. Please shoot us a text/email/phone call and we will arrange
+      accordingly.
+    </p>
+  </div>
+  <div>
+    <h2>
+      Step 3: Drive to the wedding
+    </h2>
+    <p>
+      The wedding is located at:
+    </p>
+    <p>
+      The Evergreen Lodge at Yosemite<br>
+      33160 Evergreen Rd.<br>
+      Groveland, CA 95321
+    </p>
+    <p>
+      Driving directions are available on the Evergreen Lodge
+      <a href="http://www.evergreenlodge.com/yosemite-planning/yosemite-directions-transportation/">website</a>.
+      Please keep in mind that you may not have cell service the whole drive so
+      plan ahead with directions.
+    </p>
+  </div>
+</div>
 <?
 footer();
 ?>
