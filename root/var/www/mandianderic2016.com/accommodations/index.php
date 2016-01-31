2fe81e5e89081c001e0337b8b19a1179deb695d5
Eric Spishak <emspishak@gmail.com>
2016-01-31 22:17:53 +0000

    First draft of the accomodations page.

--- root/var/www/mandianderic2016.com/accommodations/index.php
+++ root/var/www/mandianderic2016.com/accommodations/index.php
@@ -2,7 +2,142 @@
 include_once(__DIR__ . "/../template.php");
 top(Page::ACCOMMODATIONS);
 ?>
-Accommodations
+<p>
+  We encourage all guests to stay onsite
+  at <a href="http://www.evergreenlodge.com/" target="_blank">The
+  Evergreen Lodge</a> as it is where all of the wedding weekend events
+  will be taking place making it convenient and fun for
+  everyone. Think summer camp, but for adults. Please note: there is a
+  two-night minimum.
+</p>
+
+<h2>Room Types</h2>
+
+<p>
+  The Evergreen Lodge offers several different cabins:
+</p>
+
+<ul class="room-types">
+  <li>
+    <span class="room-title">Vintage Cabin</span> ($210 per night) - A
+    studio with a queen bed. Sleeps up to 2. More
+    details <a href="http://www.evergreenlodge.com/yosemite-cabins/yosemite-cabins-custom-camping/vintage-cabins/"
+    target="_blank">here</a>.
+  </li>
+  <li>
+    <span class="room-title">Deluxe Cabin</span> ($270 per night) - A
+    studio with a king bed and a queen sofa bed. Sleeps up to four
+    people. More
+    details <a href="http://www.evergreenlodge.com/yosemite-cabins/yosemite-cabins-custom-camping/deluxe-cabins/"
+    target="_blank">here</a>.
+  </li>
+  <li>
+    <span class="room-title">One Bedroom Cottage</span> ($290 per
+    night) - A king bed in the bedroom with a queen sofa bed in the
+    living room. Sleeps up to four. More
+    details <a href="http://www.evergreenlodge.com/yosemite-cabins/yosemite-cabins-custom-camping/one-bedroom-cottages/"
+    target="_blank">here</a>.
+  </li>
+  <li>
+    <span class="room-title">Two Bedroom Cabin</span> ($345 per night)
+    - A king or queen bed in one bedroom, two twin beds in the other
+    bedroom and a queen sofa bed in the living room. Sleeps up to
+    six. More
+    details <a href="http://www.evergreenlodge.com/yosemite-cabins/yosemite-cabins-custom-camping/family-cabins/"
+    target="_blank">here</a>.
+  </li>
+</ul>
+
+<p>
+  <em>All rooms include private bathrooms and private decks.</em>
+</p>
+
+<p>
+  Alternatively, custom camping is available for those of you looking
+  to get up close and personal with nature and to save a few bucks.
+</p>
+
+<ul class="room-types">
+  <li>
+    <span class="room-title">Standard Tent</span> ($95 per night) - A
+    queen foam mattress. Sleeps up to two. More
+    details <a href="http://www.evergreenlodge.com/yosemite-cabins/yosemite-cabins-custom-camping/custom-camping/"
+    target="_blank">here</a>.
+  </li>
+  <li>
+    <span class="room-title">Family Tent</span> ($130 per night) - One
+    queen and two twin full mattresses. Sleeps up to four. More
+    details <a href="http://www.evergreenlodge.com/yosemite-cabins/yosemite-cabins-custom-camping/custom-camping/"
+    target="_blank">here</a>.
+  </li>
+</ul>
+
+<p>
+  <em>Tents are spacious, fully furnished and set up before
+  arrival. Tent guests have exclusive access to a shared
+  bathhouse.</em>
+</p>
+
+<p>
+  We understand weddings can add up and highly encourage everyone to
+  shack up together to save some money and make some new friends. Most
+  of the cabins sleep 4 people, if not more, making it an easy option
+  to split the bill. Don't know anyone else coming to the wedding?
+  Don't be afraid to reach out to us! Just as we're happy to arrange
+  your carpool from the airport to the park, we're happy to arrange
+  your weekend roomies!
+</p>
+
+<p>
+  Of course if you're wanting a romantic weekend away, by all means
+  get your own cabin. The cabins are downright charming and we know
+  you'll love them as much as we do!
+</p>
+
+<h2>How to Book</h2>
+
+<p>
+  We have a room block reserved for the wedding. You must call to book
+  a room in our wedding block and get our room prices (unfortunately,
+  you cannot book online). Our room block is reserved until June 23rd
+  and we only have a limited number of rooms so please book early!
+  Trust us, there will be no availability elsewhere as Yosemite
+  National Park has campgrounds and hotels book up a year out.
+</p>
+
+<p>
+  To book, please call The Evergreen Lodge
+  at
+  <span class="desktop-only"><strong>(209) 379-2606</strong></span>
+  <span class="mobile-only"><a href="tel:209-379-2606">(209) 379-2606</a></span>
+  and tell them you'd like to book a room for the
+  Thomas/Spishak wedding on September 24, 2016.
+</p>
+
+<h2>Other Onsite Treats</h2>
+
+<p>
+  The majority of the wedding weekend will be taken care of by us
+  meaning, there is pretty much only one meal you will have to forge
+  yourself. We thought it would be helpful to list a few other
+  commodities available to you, including where to get breakfast the
+  day of the wedding!
+</p>
+
+<p>
+  <a href="http://www.evergreenlodge.com/yosemite-dining/main-lodge/"
+  target="_blank">Main Lodge Dining</a>
+</p>
+
+<p>
+  As for activities to keep you entertained when Mandi and Eric are
+  not entertaining you with their vows, dance moves, and bottomless
+  bloody marys. There is a pretty unbelievable salt water pool at the
+  lodge that offers a poolside barbeque, poolside bar, and just in
+  general a gorgeous setting to take a dip. Fall in the love with the
+  pool <a href="http://www.evergreenlodge.com/play/yosemite-swimming-pool-area/"
+  target="_blank">here</a>.
+</p>
 <?php
 footer();
 ?>
