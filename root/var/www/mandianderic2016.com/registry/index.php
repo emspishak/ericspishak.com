c93e2cffa42b0bf27e06b616a81c74847354201e
Eric Spishak <emspishak@gmail.com>
2016-03-21 04:00:03 +0000

    Add the registry page.

--- root/var/www/mandianderic2016.com/registry/index.php
+++ root/var/www/mandianderic2016.com/registry/index.php
@@ -2,7 +2,74 @@
 include_once(__DIR__ . "/../template.php");
 top(Page::REGISTRY);
 ?>
-Registry
+<p>
+  Mandi and Eric are not ashamed to admit that prior to living
+  together one might consider them "moochers". Yes, Mandi and Eric
+  consistently brought little to the table in the form of home or
+  kitchen goods when living with roommates. It worked out pretty well
+  for awhile that is until they decided to move in together. These two
+  all of a sudden were headed to Target for a $200+ trip to purchase
+  literally everything one might need for a basic kitchen and home
+  including everything from a whisk to drinking glasses. Needless to
+  say our registries below will help us to finally replace the
+  borderline dangerous items we have used over the last three years
+  (we say dangerous because we indeed had a knife snap in half while
+  we were cooking one evening).
+</p>
+
+<p>
+  <a href="http://www.heathceramics.com/giftregistry/view/index/id/5840de8f9f88e03bec23b63f67c62aba/" target="_blank">
+    <img src="/images/heath.png" alt="Heath Cereamics" class="heath-logo"/>
+  </a>
+</p>
+
+<p>
+  Mandi and Eric discovered their love for this local ceramics shop
+  shortly after moving to San Francisco. Mandi has since taken a tour
+  of their San Francisco studio learning all about their company
+  history and tile making process and has attended various events in
+  their lovely studio space. Needless to say it was a no brainer
+  deciding to register for dishes, serving bowls and platters at a
+  place so near and dear to our hearts. Consider buying local and
+  supporting our love of handmade ceramics by purchasing a registry
+  gift from Heath. We promise to have you over for dinner served on
+  their gorgeous dishes soon!
+  <a href="http://www.heathceramics.com/giftregistry/view/index/id/5840de8f9f88e03bec23b63f67c62aba/" target="_blank">Register here!</a>
+</p>
+
+<p>
+  <a href="https://secure.williams-sonoma.com/registry/crwf59w8lb/registry-list.html" target="_blank">
+    <img src="/images/williamssonoma.svg" alt="Williams-Sonoma" class="williamssonoma-logo"/>
+  </a>
+</p>
+
+<p>
+  Williams-Sonoma is one of those stores that you walk around in and
+  dream of getting married so you can register for their amazing pots,
+  pans, and beautiful cooking utensils. Oh, you don't do that, is it
+  just us? Thanks to Mandi's big sister for all of the registry
+  guidance and encouragement on going big with the All Clad
+  pans. Mandi loves to cook and maybe someday Eric will learn how so
+  we guarantee these gifts to be put to good use.
+  <a href="https://secure.williams-sonoma.com/registry/crwf59w8lb/registry-list.html" target="_blank">Register here!</a>
+</p>
+
+<p>
+  <a href="http://www.crateandbarrel.com/gift-registry/mandi-thomas-and-eric-spishak/r5475242" target="_blank">
+    <img src="/images/crateandbarrel.svg" alt="Crate and Barrel" class="crateandbarrel-logo"/>
+  </a>
+</p>
+
+<p>
+  Crate and Barrel, ole reliable. Although we became overwhelmed in
+  our registry endeavour at this monstrous store, there is nothing not
+  to love about their home goods and kitchen essentials. Mandi and
+  Eric have been slowly turning their apartment into a home that feels
+  like them and are thrilled about getting to complete their mission
+  with these perfect Crate and Barrel finds.
+  <a href="http://www.crateandbarrel.com/gift-registry/mandi-thomas-and-eric-spishak/r5475242" target="_blank">Register here!</a>
+</p>
+
 <?php
 footer(FooterIcon::CAMPFIRE, FooterIcon::LANTERN, FooterIcon::FLASHLIGHT);
 ?>
