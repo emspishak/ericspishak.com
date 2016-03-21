a06e14a6073e7c07afbfe44c3da2c5ca4eedb798
Eric Spishak <emspishak@gmail.com>
2016-03-21 04:13:21 +0000

    Add the wedding page.

--- root/var/www/mandianderic2016.com/wedding/index.php
+++ root/var/www/mandianderic2016.com/wedding/index.php
@@ -2,7 +2,104 @@
 include_once(__DIR__ . "/../template.php");
 top(Page::WEDDING);
 ?>
-Wedding!
+<p>
+  We invite you to join us for a weekend wedding celebration.
+</p>
+
+<p>
+  The weekend schedule is as follows:
+</p>
+
+<h2>Friday, September 23rd</h2>
+
+<p>
+  Guests are encouraged to arrive in the early afternoon allowing time to enjoy a group hike in the Yosemite Valley.
+</p>
+
+<h3>Welcome BBQ</h3>
+
+<p>
+  All friends and family are welcome to join
+</p>
+
+<h2>Saturday, September 24th</h2>
+
+<h3>3:30 PM Wedding Ceremony</h3>
+
+<p>
+  Reception to follow immediately after
+</p>
+
+<p>
+  10:00 PM Dance Party continues in the Tavern at the Evergreen Lodge
+</p>
+
+<h2>Sunday, September 25th</h2>
+
+<h3>Brunch with the newlyweds</h3>
+
+<p>
+  Please come say goodbye and have a bloody mary before returning home
+</p>
+
+<p>
+  All festivities will take place at the Evergreen Lodge where accomodations are available.
+</p>
+
+<h2>Wedding Party<h2>
+
+<h3>Bridesmaids</h3>
+
+<p>
+  Maid of Honor: Alex Thomas
+</p>
+
+<p>
+  Amelia Brasch
+</p>
+
+<p>
+  Beth Williams
+</p>
+
+<p>
+  Julia Paleski
+</p>
+
+<p>
+  Katie Dineen
+</p>
+
+<p>
+  Sara Contreras
+</p>
+
+<h3>Groomsmen</h3>
+
+<p>
+  Best Man: Jaysen Garcia
+</p>
+
+<p>
+  Aaron Bestick
+</p>
+
+<p>
+  Brad Bicknell
+</p>
+
+<p>
+  Griffen Latimer
+</p>
+
+<p>
+  Ryan Oman
+</p>
+
+<p>
+  Steve Geluso
+</p>
+
 <?php
 footer(FooterIcon::CAMPFIRE, FooterIcon::TENT, FooterIcon::KNOT);
 ?>
