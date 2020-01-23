f63157754e34ae990b99d94cc47db026d9a99180
Eric Spishak-Thomas <emspishak@gmail.com>
2020-01-23 02:36:46 +0000

    Support getting SSL certificates for subdomains with different webroot paths.

--- renew-certs.sh
+++ renew-certs.sh
@@ -8,15 +8,24 @@ join () {
     echo "$*"
 }
 
+domains() {
+    local domain=$1
+    local extra=$2
+    joined=$(join $domain $extra)
+    echo "--webroot-path /var/www/$domain --domains $joined"
+}
+
 renew () {
     domain=$1
     extra=$2
-    joined=$(join $domain www.$domain $extra)
+    webroots=$(domains $domain www.$domain)
+    if [ "$extra" ]; then
+        webroots="$webroots $(domains $extra)"
+    fi
     $CERTBOT_PATH/certbot-auto \
         certonly \
         --webroot \
-        --webroot-path /var/www/$domain \
-        --domains $joined
+        $webroots
 }
 
 renew "ericspishak.com" "mta-sts.ericspishak.com"
