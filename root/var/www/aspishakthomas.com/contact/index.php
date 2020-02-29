fb6ef03533396edcb4381728f37d6a6950a181ad
Eric Spishak-Thomas <emspishak@gmail.com>
2020-02-29 23:02:54 +0000

    Center and space out the Contact page.

--- root/var/www/aspishakthomas.com/contact/index.php
+++ root/var/www/aspishakthomas.com/contact/index.php
@@ -2,26 +2,28 @@
 include_once(__DIR__ . "/../template.php");
 top(Page::CONTACT);
 ?>
-<h3>Contact</h3>
+<div id="contact">
+  <h3>Contact</h3>
 
-<div>
-  Email: <a href="mailto:at2829@columbia.edu">at2829@columbia.edu</a>
-</div>
+  <div>
+    Email: <a href="mailto:at2829@columbia.edu">at2829@columbia.edu</a>
+  </div>
 
-<div>
-  Twitter: <a href="https://twitter.com/m_spishakthomas">@m_spishakthomas</a>
-</div>
+  <div>
+    Twitter: <a href="https://twitter.com/m_spishakthomas">@m_spishakthomas</a>
+  </div>
 
-<div>
-  Pronouns: She, her
-</div>
+  <div>
+    Pronouns: She, her
+  </div>
 
-<div>
-  Office Address:<br>
-  Columbia Population Research Center<br>
-  Columbia University School of Social Work<br>
-  1255 Amsterdam Avenue, 7th Floor<br>
-  New York, NY 10027
+  <div>
+    Office Address:<br>
+    Columbia Population Research Center<br>
+    Columbia University School of Social Work<br>
+    1255 Amsterdam Avenue, 7th Floor<br>
+    New York, NY 10027
+  </div>
 </div>
 <?php
 bottom();
