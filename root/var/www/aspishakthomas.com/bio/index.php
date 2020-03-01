23da404a74b138ec0f73ad749e8bc94b82352c23
Eric Spishak-Thomas <emspishak@gmail.com>
2020-03-01 00:16:15 +0000

    Add columns to the bio page.

--- root/var/www/aspishakthomas.com/bio/index.php
+++ root/var/www/aspishakthomas.com/bio/index.php
@@ -2,50 +2,54 @@
 include_once(__DIR__ . "/../template.php");
 top(Page::BIO);
 ?>
-<a href="AmandaSpishakThomas-headshot.jpg">
+<a href="AmandaSpishakThomas-headshot.jpg" class="col">
   <img
       src="AmandaSpishakThomas-headshot-small.jpg"
       class="headshot"
       alt="Amanda Spishak-Thomas headshot" />
 </a>
 
-<div>
-  Mandi Spishak-Thomas (she/her) is a doctoral student at Columbia University's
-  <a href="https://socialwork.columbia.edu">School of Social Work</a> and
-  researcher in the <a href="https://cprc.columbia.edu">Columbia Population
-  Research Center</a>. Her research interests focus broadly on health policy,
-  behavioral health, and poverty.
+<div class="col">
+  <p>
+    Mandi Spishak-Thomas (she/her) is a doctoral student at Columbia University's
+    <a href="https://socialwork.columbia.edu">School of Social Work</a> and
+    researcher in the <a href="https://cprc.columbia.edu">Columbia Population
+    Research Center</a>. Her research interests focus broadly on health policy,
+    behavioral health, and poverty.
+  </p>
+
+  <p>
+    With interests in health policy and extensive clinical social work experience,
+    Mandi has continuously sought out advocacy opportunities and ways to remain
+    engaged in the community. Mandi participated in political boot camp training
+    sponsored by the Congressional Research Institute for Social Work and Policy
+    and is an active member of the Planned Parenthood Advocacy Collective focusing
+    on political action in the women's health sphere.
+  </p>
+
+  <p>
+    Prior to beginning her doctoral studies, she worked as a licensed clinical
+    social worker at the Veterans Affairs Medical Center and Alzheimer's
+    Association in San Francisco, California and received a master's degree in
+    Social Work from Columbia University. A Southern California native and baseball
+    enthusiast (Go Mets!), Mandi enjoys hiking, biking, and running in her free
+    time.
+  </p>
 </div>
 
-<div>
-  With interests in health policy and extensive clinical social work experience,
-  Mandi has continuously sought out advocacy opportunities and ways to remain
-  engaged in the community. Mandi participated in political boot camp training
-  sponsored by the Congressional Research Institute for Social Work and Policy
-  and is an active member of the Planned Parenthood Advocacy Collective focusing
-  on political action in the women's health sphere.
+<div id="education">
+  <h3>Education</h3>
+  <dl>
+    <dt>Columbia University - New York, NY</dt>
+    <dd>Ph.D. Social Work (Expected 2022)</dd>
+
+    <dt>Columbia University - New York, NY</dt>
+    <dd>M.S.S.W. (2013)</dd>
+
+    <dt>Seattle University - Seattle, WA</dt>
+    <dd>B.A. Social Work, Nonprofit Leadership (2012)</dd>
+  </dl>
 </div>
-
-<div>
-  Prior to beginning her doctoral studies, she worked as a licensed clinical
-  social worker at the Veterans Affairs Medical Center and Alzheimer's
-  Association in San Francisco, California and received a master's degree in
-  Social Work from Columbia University. A Southern California native and baseball
-  enthusiast (Go Mets!), Mandi enjoys hiking, biking, and running in her free
-  time.
-</div>
-
-<h3>Education</h3>
-<dl>
-  <dt>Columbia University - New York, NY</dt>
-  <dd>Ph.D. Social Work (Expected 2022)</dd>
-
-  <dt>Columbia University - New York, NY</dt>
-  <dd>M.S.S.W. (2013)</dd>
-
-  <dt>Seattle University - Seattle, WA</dt>
-  <dd>B.A. Social Work, Nonprofit Leadership (2012)</dd>
-</dl>
 
 <div>
   A PDF of my most recent CV can be found
