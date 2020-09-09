360cb3f8b9a231c1711c95b941253d92fce119da
Eric Spishak-Thomas <emspishak@gmail.com>
2020-09-09 01:37:57 +0000

    Updates to the research page.

--- root/var/www/aspishakthomas.com/research/index.php
+++ root/var/www/aspishakthomas.com/research/index.php
@@ -20,20 +20,37 @@ top(Page::RESEARCH);
     Does the Earned Income Tax Credit Affect Housing Mobility among Low Income
     Workers"
   </li>
+  <li>
+    Kaplan, D., Silverstone, B., Chan, K., <strong>Spishak-Thomas, A.</strong>
+    "NASW's Supervisory Leaders in Aging: One-year Practice Change Outcomes of an
+    Innovative Training Program for Social Work Supervisors"
+  </li>
+  <li>
+    Eliason, E., <strong>Spishak-Thomas, A.</strong> "Association Between
+    Affordable Care Act Medicaid Expansions and Breastfeeding Among Low-Income
+    Women"
+  </li>
 </ul>
 
 <h3>Works in Progress</h3>
 
 <ul>
   <li>
-    Kaplan, D., Silverstone, B., Chan, K., <strong>Spishak-Thomas, A.</strong>
-    "Supervisory Leaders in Aging: One-Year Practice Change Outcomes of
-    Innovative Training Program"
-  </li>
-  <li>
     <strong>Spishak-Thomas, A.</strong>, Aivadyan, C. "'So many disincentives to
     take care of ourselves': A Photovoice study on graduate student workers'
     health care experiences"
+  </li>
+  <li>
+    <strong>Spishak-Thomas, A.</strong>, Eliason, E. "Reasons Underlying Family
+    Planning Actions among Women in the Postpartum Period"
+  </li>
+  <li>
+    Eliason, E., <strong>Spishak-Thomas, A.</strong> "Association between the
+    Affordable Care Act and Postpartum Health"
+  </li>
+  <li>
+    <strong>Spishak-Thomas, A.</strong>, Eliason, E. "Exploring Preconception
+    Family Planning Use and Pregnancy Intention among Women with a Live Birth"
   </li>
 </ul>
 
@@ -41,13 +58,26 @@ top(Page::RESEARCH);
 
 <ul>
   <li>
-    Allen, H., Zhang, Y., Eliason, E., <strong>Spishak-Thomas, A.</strong>,
+    <strong>Spishak-Thomas, A.</strong>, Slopen, M. Making Work Pay (the Rent):
+    Does the Earned Income Tax Credit Affect Housing Mobility among Low Income
+    Workers. Association for Public Policy Analysis and Management, Washington,
+    D.C., 2020. (In person meeting cancelled due to COVID-19).
+  </li>
+  <li>
+    Kaplan, D. (presenting), Silverstone, B., Chan, K., <strong>Spishak-Thomas,
+    A.</strong> NASW's Supervisory Leaders in Aging: One-year Practice Change
+    Outcomes of an Innovative Training Program for Social Work Supervisors.
+    Gerontological Society of America, Philadelphia, PA, 2020. (In person meeting
+    cancelled due to COVID-19).
+  </li>
+  <li>
+    Allen, H., Zhang, Y. (presenting), Eliason, E., <strong>Spishak-Thomas, A.</strong>,
     Frankel, C. Adverse Childhood Experiences and their Relationship with
     Depression among Adults in a Low-Income Uninsured Population. Society for
     Social Work and Research, Washington, D.C., 2020.
   </li>
   <li>
-    Price E., Garg M., <strong>Spishak-Thomas A.</strong>, Crevensten H., Ranji,
+    Price E. (presenting), Garg M., <strong>Spishak-Thomas A.</strong>, Crevensten H., Ranji,
     S. "Doctor, Your Patient Wants to Leave": Navigating Elopement Risk in
     Patients Who Lack Decision Making Capacity. Workshop at Society of Hospital
     Medicine National Meeting, Orlando, FL, 2018.
