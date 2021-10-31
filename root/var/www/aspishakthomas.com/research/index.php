846d310ac37720ea3b756f620640a494298c850e
Eric Spishak-Thomas <emspishak@gmail.com>
2021-10-31 17:09:20 +0000

    Update CV.

--- root/var/www/aspishakthomas.com/research/index.php
+++ root/var/www/aspishakthomas.com/research/index.php
@@ -6,11 +6,6 @@ top(Page::RESEARCH);
 
 <ul>
   <li>
-    Eliason, E., <strong>Spishak-Thomas, A.</strong> "Association Between
-    Affordable Care Act Medicaid Expansions and Breastfeeding Among Low-Income
-    Women"
-  </li>
-  <li>
     Kaplan, D., Silverstone, B., Chan, K., <strong>Spishak-Thomas, A.</strong>
     "NASW's Supervisory Leaders in Aging: One-year Practice Change Outcomes of an
     Innovative Training Program for Social Work Supervisors"
@@ -21,6 +16,11 @@ top(Page::RESEARCH);
 
 <ul>
   <li>
+    Eliason, E., <strong>Spishak-Thomas, A.</strong> "Impacts of the Affordable
+    Care Act Medicaid Expansions on Closely Spaced Pregnancies and Postpartum
+    Contraceptive Use"
+  </li>
+  <li>
     <strong>Spishak-Thomas, A.</strong>, Eliason, E. "Reasons Underlying Family
     Planning Actions among Women in the Postpartum Period"
   </li>
@@ -30,49 +30,78 @@ top(Page::RESEARCH);
 
 <ul>
   <li>
-    Eliason, E., <strong>Spishak-Thomas, A.</strong> "Association between the
-    Affordable Care Act and Postpartum Health"
-  </li>
-  <li>
     <strong>Spishak-Thomas, A.</strong>, Eliason, E. "Exploring Preconception
     Family Planning Use and Pregnancy Intention among Women with a Live Birth"
+  </li>
+  <li>
+    <strong>Spishak-Thomas, A.</strong>, Allen, H. "Grave Robbers: The Impact of
+    the Medicaid Estate Recovery Program on Intergenerational Transfers of Wealth
+    and Property among America's Working Families"
+  </li>
+  <li>
+    <strong>Spishak-Thomas, A.</strong> "American Women's Access to Health
+    Insurance through Midlife"
+  </li>
+  <li>
+    <strong>Spishak-Thomas, A.</strong> "Effects of Medicaid Title XIX on
+    Reproductive Health for Low-Income Women"
   </li>
 </ul>
 
 <h3>Presentations</h3>
 
+<div>
+  *presented by coauthor
+</div>
+
 <ul>
+  <li>
+    <strong>Spishak-Thomas, A.</strong>, Allen, H. Grave Robbers: The Impact of
+    the Medicaid Estate Recovery Program on Intergenerational Transfers of Wealth
+    and Property among America’s Working Families. Association for Public Policy
+    Analysis and Management, Austin, TX, 2022.
+  </li>
+  <li>
+    Eliason, E., <strong>Spishak-Thomas, A.</strong> Impacts of the Affordable
+    Care Act Medicaid Expansions on Closely Spaced Pregnancies and Postpartum
+    Contraceptive Use. Association for Public Policy Analysis and Management,
+    Austin, TX, 2022.
+  </li>
+  <li>
+    Eliason, E., <strong>Spishak-Thomas, A.</strong> Association Between
+    Affordable Care Act Medicaid Expansions and Breastfeeding among Low-Income
+    Women. Poster Presentation. American Public Health Association Annual Meeting,
+    2021 (online).*
+  </li>
   <li>
     <strong>Spishak-Thomas, A.</strong>, Aivadyan, C. 'So many disincentives to
     take care of ourselves': A Photovoice study on graduate student workers'
     health care experiences. Poster Presentation. Society for Social Work and
-    Research, San Francisco, CA, 2021. (In person meeting cancelled due to
-    COVID-19).
+    Research, San Francisco, CA, 2021 (online).
   </li>
   <li>
     <strong>Spishak-Thomas, A.</strong>, Slopen, M. Making Work Pay (the Rent):
     Does the Earned Income Tax Credit Affect Housing Mobility among Low Income
     Workers. Association for Public Policy Analysis and Management, Washington,
-    D.C., 2020. (In person meeting cancelled due to COVID-19).
+    D.C., 2020 (online).
   </li>
   <li>
-    Kaplan, D. (presenting), Silverstone, B., Chan, K., <strong>Spishak-Thomas,
+    Kaplan, D., Silverstone, B., Chan, K., <strong>Spishak-Thomas,
     A.</strong> NASW's Supervisory Leaders in Aging: One-year Practice Change
     Outcomes of an Innovative Training Program for Social Work Supervisors.
-    Gerontological Society of America, Philadelphia, PA, 2020. (In person meeting
-    cancelled due to COVID-19).
+    Gerontological Society of America, Philadelphia, PA, 2020 (online).*
   </li>
   <li>
-    Allen, H., Zhang, Y. (presenting), Eliason, E., <strong>Spishak-Thomas, A.</strong>,
+    Allen, H., Zhang, Y., Eliason, E., <strong>Spishak-Thomas, A.</strong>,
     Frankel, C. Adverse Childhood Experiences and their Relationship with
     Depression among Adults in a Low-Income Uninsured Population. Society for
-    Social Work and Research, Washington, D.C., 2020.
+    Social Work and Research, Washington, D.C., 2020.*
   </li>
   <li>
-    Price E. (presenting), Garg M., <strong>Spishak-Thomas A.</strong>, Crevensten H., Ranji,
+    Price E., Garg M., <strong>Spishak-Thomas A.</strong>, Crevensten H., Ranji,
     S. "Doctor, Your Patient Wants to Leave": Navigating Elopement Risk in
     Patients Who Lack Decision Making Capacity. Workshop at Society of Hospital
-    Medicine National Meeting, Orlando, FL, 2018.
+    Medicine National Meeting, Orlando, FL, 2018.*
   </li>
 </ul>
 <?php
